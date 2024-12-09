ee74ee840e7000306c382aa1bed3e0691e936b49
johnmapengu <183103110+johnmapengu@users.noreply.github.com>
2024-12-09 13:16:10 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def open_window_manchine():
+    window_manchine = tk.Toplevel
+    window_manchine = title("atm")
+    window_manchine = geometry("350x500")
+
+    screen = tk.label(window_manchine)
+
+    Screen.pack(pady=10)
